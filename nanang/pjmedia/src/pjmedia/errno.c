0e36e8739b560c2e871d259b0e0a0af226e4fc88
nanang <nanang@bdd19603-ad22-4b21-bbe5-5c27d6d023c3>
2008-01-18 20:14:17 +0000

    Ticket #452: Updated errno.h & .c to accomodate SRTP Transport & libsrtp error code
    git-svn-id: file:///mirror/SvnSyncs/PJ/repos/pjproject/branches/users@1710 bdd19603-ad22-4b21-bbe5-5c27d6d023c3

--- nanang/pjmedia/src/pjmedia/errno.c
+++ nanang/pjmedia/src/pjmedia/errno.c
@@ -23,6 +23,9 @@
 #   include <portaudio.h>
 #endif
 
+#if defined(PJMEDIA_HAS_SRTP) && (PJMEDIA_HAS_SRTP != 0)
+    pj_str_t srtp_transport_getliberrstr(int err);
+#endif
 
 
 /* PJMEDIA's own error codes/messages 
@@ -143,9 +146,16 @@ static const struct
     PJ_BUILD_ERR( PJMEDIA_ESNDINSAMPLEFMT,  "Invalid sample format for sound device" ),
 
     /* SRTP transport errors: */
-    PJ_BUILD_ERR( PJMEDIA_SRTP_ESDPREQSECTP,   "Secure transport required in SDP media descriptor" ),
-    PJ_BUILD_ERR( PJMEDIA_SRTP_ESDPAMBIGUEANS, "SDP contains ambigue answer" ),
-    
+    PJ_BUILD_ERR( PJMEDIA_SRTP_ECRYPTONOTMATCH, "SRTP crypto-suite name not match the offerer tag" ),
+    PJ_BUILD_ERR( PJMEDIA_SRTP_EINKEYLEN,	"Invalid SRTP key length for specific crypto" ),
+    PJ_BUILD_ERR( PJMEDIA_SRTP_ENOTSUPCRYPTO,   "Unsupported SRTP crypto-suite" ),
+    PJ_BUILD_ERR( PJMEDIA_SRTP_ESDPAMBIGUEANS,  "SRTP SDP contains ambigue answer" ),
+    PJ_BUILD_ERR( PJMEDIA_SRTP_ESDPDUPCRYPTOTAG,"Duplicated SRTP crypto tag" ),
+    PJ_BUILD_ERR( PJMEDIA_SRTP_ESDPINCRYPTO,    "Invalid SRTP crypto attribute" ),
+    PJ_BUILD_ERR( PJMEDIA_SRTP_ESDPINCRYPTOTAG, "Invalid SRTP crypto tag" ),
+    PJ_BUILD_ERR( PJMEDIA_SRTP_ESDPINTRANSPORT, "Invalid SDP media transport for SRTP" ),
+    PJ_BUILD_ERR( PJMEDIA_SRTP_ESDPREQCRYPTO,   "SRTP crypto attribute required in SDP" ),
+    PJ_BUILD_ERR( PJMEDIA_SRTP_ESDPREQSECTP,    "Secure transport required in SDP media descriptor" )
 };
 
 #endif	/* PJ_HAS_ERROR_STRING */
@@ -164,11 +174,12 @@ PJ_DEF(pj_str_t) pjmedia_strerror( pj_status_t statcode,
 
     /* See if the error comes from PortAudio. */
 #if PJMEDIA_SOUND_IMPLEMENTATION==PJMEDIA_SOUND_PORTAUDIO_SOUND
-    if (statcode >= PJMEDIA_ERRNO_FROM_PORTAUDIO(paNotInitialized) &&
-	statcode <  PJMEDIA_ERRNO_FROM_PORTAUDIO(paNotInitialized + 10000))
+    if (statcode >= PJMEDIA_PORTAUDIO_ERRNO_START &&
+	statcode <= PJMEDIA_PORTAUDIO_ERRNO_END)
     {
 
-	int pa_err = statcode - PJMEDIA_ERRNO_FROM_PORTAUDIO(0);
+	//int pa_err = statcode - PJMEDIA_ERRNO_FROM_PORTAUDIO(0);
+	int pa_err = PJMEDIA_PORTAUDIO_ERRNO_START - statcode;
 	pj_str_t msg;
 	
 	msg.ptr = (char*)Pa_GetErrorText(pa_err);
@@ -180,8 +191,27 @@ PJ_DEF(pj_str_t) pjmedia_strerror( pj_status_t statcode,
 
     } else 
 #endif	/* PJMEDIA_SOUND_IMPLEMENTATION */
+
+#if defined(PJMEDIA_HAS_SRTP) && (PJMEDIA_HAS_SRTP != 0)
+    /* LIBSRTP error */
+    if (statcode >= PJMEDIA_LIBSRTP_ERRNO_START &&
+	statcode <  PJMEDIA_LIBSRTP_ERRNO_END)
+    {
+	int err = statcode - PJMEDIA_PORTAUDIO_ERRNO_START;
+	pj_str_t msg;
+	
+	msg = srtp_transport_getliberrstr(err);
+
+	errstr.ptr = buf;
+	pj_strncpy_with_null(&errstr, &msg, bufsize);
+	return errstr;
+    
+    } else
+#endif
+    
+    /* PJMEDIA error */
     if (statcode >= PJMEDIA_ERRNO_START && 
-	       statcode < PJMEDIA_ERRNO_START + PJ_ERRNO_SPACE_SIZE)
+	       statcode < PJMEDIA_ERRNO_END)
     {
 	/* Find the error in the table.
 	 * Use binary search!
@@ -216,8 +246,7 @@ PJ_DEF(pj_str_t) pjmedia_strerror( pj_status_t statcode,
 	    return errstr;
 
 	} 
-    }
-
+    } 
 #endif	/* PJ_HAS_ERROR_STRING */
 
     /* Error not found. */
